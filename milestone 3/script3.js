2482016b06091d354090b94190cd57f0fa5aa937
silviawolves <silviacanepa.w@gmail.com>
2022-06-15 14:17:12 +0200

    milestone 3 + consegna

--- milestone 3/script3.js
+++ milestone 3/script3.js
@@ -181,7 +181,7 @@ new Vue({
 
 
 /*
-Milestone 2
-- Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione X
-- Click sul contatto mostra la conversazione del contatto cliccato X
+Milestone 3
+- Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+- Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 */
